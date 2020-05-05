2d415b92134f60d31448efa74a133685f3308bdc
EndePointe <endepointe@gmail.com>
2020-05-06 00:27:31 +0100

    test

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.set('port', port);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-//app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '/../build')));
 
 app.use(cors());
 
