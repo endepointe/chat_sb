192a7d1b0a9a39188e9d1cc7b79604eb684c6bf0
EndePointe <endepointe@gmail.com>
2020-05-06 14:20:56 +0100

    api test

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,13 @@ import Chat from './comps/chat/Chat';
 //<button onClick={getAPI}>get api</button>
 const App = () => {
   const getAPI = () => {
-    fetch('http://localhost:3001').then(res => res.json()).then((result) => console.log(result));
+    fetch('http://localhost:3000').then(res => res.json()).then((result) => console.log(result));
   };
 
   return (
     <div>
       <Chat />
-      <button onClick={getAPI}>get api</button>
+      <button onClick={getAPI}>get api result in console</button>
     </div>
   );
 }
